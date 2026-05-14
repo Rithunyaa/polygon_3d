004fc41c55b2abb825fd150797c974401638c8bc
BlaBlahBlahh <rithunyaa@gmail.com>
2026-05-14 22:17:22 +0000

    Added torus with texture

--- main.js
+++ main.js
@@ -7,20 +7,31 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+// cube
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const texture = new THREE.TextureLoader().load( 'assets/earth.png' );
 const material = new THREE.MeshBasicMaterial({ map: texture });
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 5;
+// donut
+const donut_geo = new THREE.TorusGeometry(10, 3, 16, 100);
+const donut_tex = new THREE.TextureLoader().load( 'assets/galazy_polygon.png' );
+const donut_mat = new THREE.MeshBasicMaterial({ map: donut_tex });
+const donut = new THREE.Mesh(donut_geo, donut_mat);
+scene.add(donut);
+
+camera.position.z = 18;
 
 function animate() {
   requestAnimationFrame( animate );
   cube.rotation.x += 0.03;
   cube.rotation.y += 0.03;
+  donut.rotation.x += 0.05;
+  donut.rotation.y += 0.05;
   renderer.render( scene, camera );
 }
 
 animate();
+
 
