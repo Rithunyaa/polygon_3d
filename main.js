a48e483229149c7f610fdbc8160da8cdfb7c04e7
BlaBlahBlahh <rithunyaa@gmail.com>
2026-05-14 22:05:12 +0000

    Made animation a bit faster

--- main.js
+++ main.js
@@ -17,8 +17,8 @@ camera.position.z = 5;
 
 function animate() {
   requestAnimationFrame( animate );
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  cube.rotation.x += 0.03;
+  cube.rotation.y += 0.03;
   renderer.render( scene, camera );
 }
 
