92f0d6f68cf0722964eba404ed42dd4bbc4666ae
BlaBlahBlahh <rithunyaa@gmail.com>
2026-05-14 22:03:33 +0000

    Debugged and added texture

--- main.js
+++ main.js
@@ -8,25 +8,8 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
-camera.position.z = 5;
-
-function animate() {
-  requestAnimationFrame( animate ); // add this line
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
-  renderer.render( scene, camera );
-}
-
-animate();
-
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const texture = new THREE.TextureLoader().load('zaid.png')
-const material = new THREE.MeshBasicMaterial({map: texture});
+const texture = new THREE.TextureLoader().load( 'assets/earth.png' );
+const material = new THREE.MeshBasicMaterial({ map: texture });
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
@@ -36,8 +19,8 @@ function animate() {
   requestAnimationFrame( animate );
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
-
   renderer.render( scene, camera );
-
 }
+
+animate();
 
