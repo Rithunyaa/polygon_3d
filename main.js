8f671872cb3bf9bf01252207b634edb74286a16b
BlaBlahBlahh <rithunyaa@gmail.com>
2026-05-14 22:25:22 +0000

    Made css file

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
+import './style.css';
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
 renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setAnimationLoop( animate );
+
+renderer.render(scene, camera);
+
 
 // cube
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const geometry = new THREE.BoxGeometry( 3, 3, 3 );
 const texture = new THREE.TextureLoader().load( 'assets/earth.png' );
 const material = new THREE.MeshBasicMaterial({ map: texture });
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 // donut
-const donut_geo = new THREE.TorusGeometry(10, 3, 16, 100);
+const donut_geo = new THREE.TorusGeometry(6, 2, 16, 100);
 const donut_tex = new THREE.TextureLoader().load( 'assets/galazy_polygon.png' );
 const donut_mat = new THREE.MeshBasicMaterial({ map: donut_tex });
 const donut = new THREE.Mesh(donut_geo, donut_mat);
@@ -34,4 +41,18 @@ function animate() {
 
 animate();
 
+function add_star() {
+  const star_geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const star_material = new THREE.MeshBasicMaterial({ color: 0xffffff  });
+  const star = new THREE.Mesh(star_geometry, star_material);
+
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(200));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+Array(200).fill().forEach(add_star);
 
